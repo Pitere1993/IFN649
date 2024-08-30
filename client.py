8652a747bd135e450aa33cd9094bc65e3d689e7e
Pitere1993 <n11685719@qut.edu.au>
2024-08-30 22:33:52 +1000

    Add files via upload

---
+++ client.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+import serial
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT")
+    print("Connection returned result: " + str(rc))
+
+    client.subscribe("ifn649")
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    ser = serial.Serial("/dev/rfcomm0", 9600)
+    ser.write(msg.payload)
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("3.27.23.183", 1883, 30)
+
+client.loop_forever()
